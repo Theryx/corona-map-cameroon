81cf60eb1501f4a300379facb4701585147e990c
iamYannickD <yannickdoungmo@gmail.com>
2020-04-09 14:09:22 +0100

    file restructuration

--- m.dashboard/php/Sud_sum.php
+++ m.dashboard/php/Sud_sum.php
@@ -1,7 +1,7 @@
 <?php
 
     $db = new PDO('pgsql:host=georesponse.postgres.database.azure.com;port=5432;dbname=georesponsedb;', 'postgres@georesponse', 'Geoadvantage2020');
-    $sql = $db->prepare("select nombre_total_de_cas, nombre_de_victimes,nombre_de_personnes_retablies from region where name = 'Centre'");
+    $sql = $db->prepare("select nombre_total_de_cas, nombre_de_victimes,nombre_de_personnes_retablies from region where name = 'Sud'");
     $sql->execute();
         
 
