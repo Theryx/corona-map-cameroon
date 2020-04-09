2152d103cb04c04bc4eea0fc911ecf7dad5f497a
iamYannickD <yannickdoungmo@gmail.com>
2020-04-09 10:35:40 +0100

    added stats cals for south region

---
+++ dashboard/php/ouest_sum.php
@@ -0,0 +1,14 @@
+<?php
+
+    $db = new PDO('pgsql:host=georesponse.postgres.database.azure.com;port=5432;dbname=georesponsedb;', 'postgres@georesponse', 'Geoadvantage2020');
+    $sql = $db->prepare("select nombre_total_de_cas, nombre_de_victimes,nombre_de_personnes_retablies from region where name = 'Ouest'");
+    $sql->execute();
+        
+
+    while ($row = $sql->fetch(PDO::FETCH_ASSOC)) {
+
+         echo json_encode($row);
+    }
+   
+ 
+?>
